4db15ddafec1ae10f1c5f3d651325c3d94675791
pittayut khorniewklang <pittayut.k@ku.th>
2025-09-30 00:39:33 +0700

    menu-home_UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent}
 ];
 
 @NgModule({
